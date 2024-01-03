97329d25dfb642b4a983b587b056edb14a224f70
loreloc <lorenzoloconte@outlook.it>
2024-01-03 17:56:34 +0100

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+stack ghc -- site.hs -threaded || exit 1
+stack exec ./site rebuild || exit 1
+git switch --orphan deploy && git add _site && git commit -m "added _site" || exit 1
+git branch -D pages && git subtree split -P _site -b pages || exit 1
+git checkout pages && git branch -D deploy || exit 1
+git push origin pages --force && git checkout main
