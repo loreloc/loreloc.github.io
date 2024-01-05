b68cede0c7288c5739b9d5f8f8e34518b5c01f4f
loreloc <lorenzoloconte@outlook.it>
2024-01-05 10:42:30 +0100

    fix site.hs

--- site.hs
+++ site.hs
@@ -14,6 +14,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "authors/**" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "js/*" $ do
         route   idRoute
         compile copyFileCompiler
