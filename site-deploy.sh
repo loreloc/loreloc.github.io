68691b1cb6b699ee8893b80fd65e719d39db97b7
loreloc <lorenzoloconte@outlook.it>
2024-01-07 18:18:59 +0100

    updated site-deploy script

--- site-deploy.sh
+++ site-deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+git checkout main
+
 stack build && stack exec site rebuild || exit 1
+
 git switch --orphan deploy && git add _site && git commit -m "added _site" || exit 1
-git branch -D pages && git subtree split -P _site -b pages || exit 1
-git checkout pages && git branch -D deploy || exit 1
-git push origin pages --force && git checkout main
+git subtree split -P _site -b pages && git checkout pages || exit 1
+
+git push origin pages --force && git checkout main || exit 1
+
+git branch -D deploy &>/dev/null
+git branch -D pages &>/dev/null
